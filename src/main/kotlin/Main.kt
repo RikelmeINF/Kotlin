1f6443102cae34467e635a2e9bffa1609a91fffb
RikelmeINF <rikelme.inf@gmail.com>
2023-07-23 14:09:29 -0300

    explicação do codigo de interação com usuário

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main (){
+    println("Qual o seu nome ?")
+    val name = readln()
+    println("Hello, $name!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
+// Código acima é uma pequena interação com usuário.
+// (println) serve para demonstar.
+// (readln) serve para armazenar texto dado pelo usuário.
+// (val) serve para atribuir uma "váriavel".
+// Para chamar uma variavel é importante a aplicação do $ no início.
